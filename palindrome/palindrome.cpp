9aa7cc873536b33416672be5398136c9d9bae500
Kauan <kauanrossii>
2023-12-20 12:33:21 -0300

    Palindrome exercise refactored

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
+bool IsPalindrome(char string[], int size) {
+    bool palindrome = true;
+    int sizeWithoutNullCharacter = size -1;
+    int indexBackingLetter = sizeWithoutNullCharacter -1;
+
+    for (int i = 0; i < sizeWithoutNullCharacter; i++) {
+        if (string[i] != string[indexBackingLetter]) palindrome = false;
+        indexBackingLetter--;
+    }
+    return palindrome;
+}
+
 int main() {
     char string[4] = "ALO";
-    bool palindrome = true;
-    int backingLetter = 2;
-
-    for (int i = 0; i < 3; i++) {
-        if (string[i] != string[backingLetter]) palindrome = false;
-        backingLetter--;
-    }
-
-    std::cout << "Palindromo: " << palindrome;
-
+    std::cout << "Palindrome: " << IsPalindrome(string, 4);
     return 0;
 }
